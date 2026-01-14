622dd32f2b587a36375033b745dd58f083368f21
Paul <paulpep2002@gmail.com>
2026-01-14 11:06:52 +0300

    updated eas cli+ align eas update settings

--- apps/mobile/app.config.js
+++ apps/mobile/app.config.js
@@ -50,7 +50,7 @@ export default function expoConfig({ config }) {
 
     name: 'DayBreak',
     slug: 'funzasasa',
-    version: '1.0.5',
+    version: '1.0.6',
     scheme: 'daybreak',
 
     runtimeVersion: { policy: 'appVersion' },
