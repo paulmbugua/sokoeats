8f75b067de2e8fc44bbb8b5598ffcbe670fa0872
Paul <paulpep2002@gmail.com>
2026-07-29 15:05:01 +0300

    Set production static env for web and admin

--- apps/admin/public/env.js
+++ apps/admin/public/env.js
@@ -1,2 +1,8 @@
-window.__EKAZI_ENV__ = window.__EKAZI_ENV__ || {};
-window.__BACKEND_URL__ = window.__BACKEND_URL__ || window.__EKAZI_ENV__.VITE_BACKEND_URL || '';
+window.__EKAZI_ENV__ = Object.assign({}, window.__EKAZI_ENV__ || {}, {
+  VITE_BACKEND_URL: 'https://server.ekazi.co.ke',
+  VITE_API_URL: 'https://server.ekazi.co.ke',
+  VITE_SITE_URL: 'https://admin.ekazi.co.ke',
+  VITE_IMAGES_BASE_URL: 'https://images.ekazi.co.ke',
+  VITE_PREVIEWS_BASE_URL: 'https://previews.ekazi.co.ke',
+});
+window.__BACKEND_URL__ = window.__EKAZI_ENV__.VITE_BACKEND_URL || window.__EKAZI_ENV__.VITE_API_URL || 'https://server.ekazi.co.ke';
--- apps/web/public/env.js
+++ apps/web/public/env.js
@@ -1,2 +1,9 @@
-window.__EKAZI_ENV__ = window.__EKAZI_ENV__ || {};
-window.__BACKEND_URL__ = window.__BACKEND_URL__ || window.__EKAZI_ENV__.VITE_BACKEND_URL || '';
+window.__EKAZI_ENV__ = Object.assign({}, window.__EKAZI_ENV__ || {}, {
+  VITE_BACKEND_URL: 'https://server.ekazi.co.ke',
+  VITE_API_URL: 'https://server.ekazi.co.ke',
+  VITE_SITE_URL: 'https://ekazi.co.ke',
+  VITE_GOOGLE_WEB_CLIENT_ID: '912636242362-m5hogktgcnramtb6g132aada1jftsfrl.apps.googleusercontent.com',
+  VITE_IMAGES_BASE_URL: 'https://images.ekazi.co.ke',
+  VITE_PREVIEWS_BASE_URL: 'https://previews.ekazi.co.ke',
+});
+window.__BACKEND_URL__ = window.__EKAZI_ENV__.VITE_BACKEND_URL || window.__EKAZI_ENV__.VITE_API_URL || 'https://server.ekazi.co.ke';
